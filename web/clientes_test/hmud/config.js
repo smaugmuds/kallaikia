02235a676b8f277b18fcb20baac23a15180033f7
Antonio Cao <antoniocao000@gmail.com>
2014-03-17 21:14:03 +0100

    Publicado desde Makefile

--- web/clientes_test/hmud/config.js
+++
@@ -1,8 +0,0 @@
-var HMUD_Config = {
-             host: window.location.hostname,
-             port: 4010,
-       policyPort: 843,
-   maxHistorySize: 100,
- historyMinLength: 3
-};
-
--- web/clientes_test/hmud/flashpolicyd_v0.6/Python_init/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-install -m644 ../flashpolicy.xml /usr/local/etc/
-
-install -m755 flashpolicyd.py /usr/local/sbin/
-install -m755 flashpolicyd.init /etc/rc.d/init.d/flashpolicyd
-chkconfig --add flashpolicyd
